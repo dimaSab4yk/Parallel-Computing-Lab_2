b39ce8784554149643be890cb9fc1d2a427bcafa
DimaCoder1 <147308935+DimaCoder1@users.noreply.github.com>
2025-03-23 22:04:30 +0200

    Додаємо реалізацію завдання з використанням атомарних змінних та операцій

--- Lab_2.cpp
+++ Lab_2.cpp
@@ -5,65 +5,114 @@
 #include <thread>
 #include <vector>
 #include <ctime>
-#include <mutex>
+#include <mutex> 
 
 using namespace std;
 using namespace std::chrono;
 
-void searchNumber(vector<int>& matrix, int start, int end, int& localCount, int& localMin);
-void divisionParts(vector<int>& matrix, int numberThreads, int& count, int& minNumber);
 
-void matrixFilling(vector<int>& matrix)
+void searchNumberMutex(vector<int>& matrix, int start, int end, int& count, int& minNumber, mutex& mtx);
+void divisionPartsMutex(vector<int>& matrix, int numberThreads, int& count, int& minNumber, mutex& mtx);
+void searchNumberCAS(vector<int>& matrix, int start, int end, atomic<int>& count, atomic<int>& minNumber);
+void divisionPartsCAS(vector<int>& matrix, int numberThreads, atomic<int>& count, atomic<int>& minNumber);
+
+void matrixFilling(vector<int>& matrix, int size)
 {
-    for (int& num : matrix)
+    for (int i = 0; i < size; i++)
     {
-        num = rand() % 1000;
+        matrix[i] = 21 + rand() % 1000;
     }
 }
 
-void searchNumber(vector<int>& matrix, int start, int end, int& localCount, int& localMin)
+void searchNumber(vector<int>& matrix, int size, int& count, int& minNumber)
 {
-    for (int i = start; i < end; i++)
+    for (int i = 0; i < size; i++)
     {
         if (matrix[i] % 21 == 0)
         {
-            localCount++;
-            if (matrix[i] < localMin)
+            count++;
+
+            if (matrix[i] < minNumber)
             {
-                localMin = matrix[i];
+                minNumber = matrix[i];
             }
         }
     }
 }
 
-void divisionParts(vector<int>& matrix, int numberThreads, int& count, int& minNumber)
+void divisionPartsMutex(vector<int>& matrix, int numberThreads, int& count, int& minNumber, mutex& mtx)
 {
     int chunkSize = matrix.size() / numberThreads;
     vector<thread> threads;
-    vector<int> localCounts(numberThreads, 0);
-    vector<int> localMins(numberThreads, INT_MAX);
 
     for (int i = 0; i < numberThreads; i++)
     {
         int start = i * chunkSize;
-        int end = (i == numberThreads - 1) ? matrix.size() : (i + 1) * chunkSize;
+        int end = (i + 1) * chunkSize;
 
-        threads.emplace_back(searchNumber, ref(matrix), start, end, ref(localCounts[i]), ref(localMins[i]));
+        if (i == numberThreads - 1)
+        {
+            end = matrix.size();
+        }
+
+        threads.emplace_back(searchNumberMutex, ref(matrix), start, end, ref(count), ref(minNumber), ref(mtx));
     }
 
     for (auto& t : threads)
     {
         t.join();
     }
+}
 
-    count = 0;
-    minNumber = INT_MAX;
-    for (int i = 0; i < numberThreads; i++)
+void searchNumberMutex(vector<int>& matrix, int start, int end, int& count, int& minNumber, mutex& mtx)
+{
+    for (int i = start; i < end; i++)
     {
-        count += localCounts[i];
-        if (localMins[i] < minNumber)
+        if (matrix[i] % 21 == 0)
         {
-            minNumber = localMins[i];
+            mtx.lock();
+            count++;
+            if (matrix[i] < minNumber)
+            {
+                minNumber = matrix[i];
+            }
+            mtx.unlock();
+        }
+    }
+}
+
+void divisionPartsCAS(vector<int>& matrix, int numberThreads, atomic<int>& count, atomic<int>& minNumber) 
+{
+    int chunkSize = matrix.size() / numberThreads;
+    vector<thread> threads;
+
+    for (int i = 0; i < numberThreads; i++) 
+    {
+        int start = i * chunkSize;
+        int end = (i + 1) * chunkSize;
+
+        if (i == numberThreads - 1)
+        {
+            end = matrix.size();
+        }
+        threads.emplace_back(searchNumberCAS, ref(matrix), start, end, ref(count), ref(minNumber));
+    }
+    for (auto& t : threads)
+    {
+        t.join();
+    }
+}
+
+void searchNumberCAS(vector<int>& matrix, int start, int end, atomic<int>& count, atomic<int>& minNumber) 
+{
+    for (int i = start; i < end; i++) 
+    {
+        if (matrix[i] % 21 == 0) 
+        {
+            count.fetch_add(1, memory_order_relaxed);
+            int oldMin = minNumber.load(memory_order_relaxed);
+
+            while (matrix[i] < oldMin && !minNumber.compare_exchange_weak(oldMin, matrix[i], memory_order_relaxed));
         }
     }
 }
@@ -72,53 +121,69 @@ int main()
 {
     srand(time(0));
 
-    vector<int> sizeMatrix = { 100000, 500000, 1000000, 5000000, 30000000, 90000000 };
+    vector<int> sizeMatrix = {100000, 500000, 1000000, 5000000, 30000000, 90000000};
     vector<int> numberThreads = { 1, 4, 8, 16, 32, 64, 128, 256 };
 
-    for (int size : sizeMatrix)
+    for (int i = 0; i < sizeMatrix.size(); i++)
     {
-        vector<int> matrix(size);
-        matrixFilling(matrix);
+        vector<int> matrix(sizeMatrix[i]);
+
+        matrixFilling(matrix, sizeMatrix[i]);
 
         int count = 0, minNumber = INT_MAX;
 
         auto start = high_resolution_clock::now();
-        searchNumber(matrix, 0, size, count, minNumber);
-        auto end = high_resolution_clock::now();
+        searchNumber(matrix, sizeMatrix[i], count, minNumber);
 
+        auto end = high_resolution_clock::now();
         auto duration = duration_cast<nanoseconds>(end - start).count();
         double seconds = duration * 1e-9;
 
-        cout << "\nSize matrix: " << size;
-        cout << "\n   - Work time: " << fixed << seconds << " seconds";
-        if (count > 0)
-            cout << "\n   - Count: " << count << ", Min number: " << minNumber << endl;
-        else
-            cout << "\n   - Count: " << count << ", No numbers divisible by 21 found." << endl;
+        cout << "\nSize matrix: " << sizeMatrix[i];
+        cout << "\n   - Work time: " << fixed << seconds << " seconds" << endl;
+        cout << "\n   - Count: " << count << ", Min number: " << minNumber << endl;
     }
 
-    for (int size : sizeMatrix)
+    for (int i = 0; i < sizeMatrix.size(); i++)
     {
-        vector<int> matrix(size);
-        matrixFilling(matrix);
+        vector<int> matrix(sizeMatrix[i]);
+
+        matrixFilling(matrix, sizeMatrix[i]);
 
         for (int numThreads : numberThreads)
         {
             int count = 0, minNumber = INT_MAX;
+            mutex mtx;
 
             auto start = high_resolution_clock::now();
-            divisionParts(matrix, numThreads, count, minNumber);
+            divisionPartsMutex(matrix, numThreads, count, minNumber, mtx);
             auto end = high_resolution_clock::now();
 
             auto duration = duration_cast<nanoseconds>(end - start).count();
             double seconds = duration * 1e-9;
 
-            cout << "\nSize matrix: " << size << " with " << numThreads << " threads";
+            cout << "\nSize matrix: " << sizeMatrix[i] << " with " << numThreads << " threads";
             cout << "\n   - Work time: " << fixed << seconds << " seconds";
-            if (count > 0)
-                cout << "\n   - Count: " << count << ", Min number: " << minNumber << endl;
-            else
-                cout << "\n   - Count: " << count << ", No numbers divisible by 21 found." << endl;
+            cout << "\n   - Count: " << count << ", Min number: " << minNumber << endl;
+        }
+    }
+
+    for (int i = 0; i < sizeMatrix.size(); i++) 
+    {
+        vector<int> matrix(sizeMatrix[i]);
+        matrixFilling(matrix, sizeMatrix[i]);
+
+        for (int numThreads : numberThreads) 
+        {
+            atomic<int> count(0), minNumber(INT_MAX);
+
+            auto start = high_resolution_clock::now();
+            divisionPartsCAS(matrix, numThreads, count, minNumber);
+            auto end = high_resolution_clock::now();
+
+            cout << "\nSize matrix: " << sizeMatrix[i] << " with " << numThreads << " threads (CAS)";
+            cout << "\n   - Work time: " << duration_cast<nanoseconds>(end - start).count() * 1e-9 << " seconds";
+            cout << "\n   - Count: " << count << ", Min number: " << minNumber << endl;
         }
     }
 
